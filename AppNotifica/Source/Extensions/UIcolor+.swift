dcaf94bfa1e43491ab2caf4c2e391e0ef065f312
felipehenriquesouza <117363096+felipehenriquesouza@users.noreply.github.com>
2022-11-17 11:53:55 -0300

    dark mode
    dark mode

---
+++ AppNotifica/Source/Extensions/UIcolor+.swift
@@ -0,0 +1,35 @@
+//
+//  UIcolor+.swift
+//  AppNotifica
+//
+//  Created by IFB BIOTIC 17 on 17/11/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static var viewBackGroundColor: UIColor {
+        UIColor(named: "viewBackGroundColor") ??
+            .white
+    }
+    static var textColor: UIColor {
+        UIColor(named: "textColor") ??
+            .white
+    }
+    static var buttonColor: UIColor {
+        UIColor(named: "buttonColor") ??
+            .white
+    }
+    
+}
+
+
+
+
+
+
+
+
+
